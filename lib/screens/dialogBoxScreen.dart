d7b1a5f2ea41dad68ce3bbe8c24abe0afbe16977
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-05-18 15:31:57 +0530

    Implemented Different DialogBox

--- lib/screens/dialogBoxScreen.dart
+++ lib/screens/dialogBoxScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../widget/simpleDialog.dart' as SimpleDialogBox;
+import '../widget/customDialog.dart' as customDialogBox;
+import '../widget/complexDialog.dart' as compledDialogBox;
 
 class DialogBox extends StatelessWidget {
   static const routeName = "/DialogBoxScreen";
@@ -8,6 +11,34 @@ class DialogBox extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Dialog Box"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.one_k),
+              tooltip: "Simple Dialog Box",
+              iconSize: 100.0,
+              onPressed: () => SimpleDialogBox.showSimpleDialog(context),
+              color: Colors.blueAccent,
+            ),
+            IconButton(
+              icon: Icon(Icons.two_k),
+              tooltip: "Simple Custom Dialog Box",
+              iconSize: 100.0,
+              onPressed: () => customDialogBox.showCustomDialogBox(context),
+              color: Colors.redAccent,
+            ),
+            IconButton(
+              icon: Icon(Icons.three_k),
+              tooltip: "Complex Custom Dialog Box",
+              iconSize: 100.0,
+              onPressed: () => compledDialogBox.showSimpleDialog(context),
+              color: Colors.pinkAccent,
+            ),
+          ],
+        ),
       ),
     );
   }
